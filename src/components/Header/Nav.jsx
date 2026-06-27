949d5d4403e87c0a83b186b3876c311aa0dfa61b
nur alam joy <ajnuralam@gmail.com>
2026-06-27 21:04:26 +0600

    update Nav.jsx

--- src/components/Header/Nav.jsx
+++ src/components/Header/Nav.jsx
@@ -35,7 +35,7 @@ const Nav = () => {
             ))}
           </ul>
 
-          {/* Desktop Button */}
+          {/* ====Desktop Button==== */}
           <button className="hidden md:block bg-blue-600 text-white px-5 py-2 rounded-full hover:bg-blue-700 transition duration-300">
             Hire Me
           </button>
