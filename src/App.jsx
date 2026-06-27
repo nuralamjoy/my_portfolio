4ec3420b8ed453247474ba884650aff6a9f2b1ab
nur alam joy <ajnuralam@gmail.com>
2026-06-27 21:01:30 +0600

    update app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ const App = () => {
   return (
     <>
       <Nav />
-      {/* Dummy Content */}
+      
       <div className="pt-20">
         <section className="h-screen flex items-center justify-center bg-gray-100">
           <h1 className="text-5xl font-bold">Home Section</h1>
