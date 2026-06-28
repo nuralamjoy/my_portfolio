a08dd645e362b394b784eb8013b6324aed281156
nur alam joy <ajnuralam@gmail.com>
2026-06-28 22:26:08 +0600

    grid animation

--- src/components/BodyBackground/Background.jsx
+++ src/components/BodyBackground/Background.jsx
@@ -1,16 +1,37 @@
+import "./Background.css";
 
-const Background = () => {
+const stars = Array.from({ length: 80 });
+
+export default function Background() {
   return (
-    <>
-      <div className="fixed inset-0 -z-10 overflow-hidden bg-[#050010]">
-        <div className="grid-bg"></div>
+    <div className="background">
 
-        <div className="orb orb1"></div>
-        <div className="orb orb2"></div>
-        <div className="orb orb3"></div>
+      <div className="gradient"></div>
+
+      <div className="grid"></div>
+
+      <div className="orb orb1"></div>
+      <div className="orb orb2"></div>
+      <div className="orb orb3"></div>
+      <div className="orb orb4"></div>
+
+      <div className="stars">
+        {stars.map((_, i) => (
+          <span
+            key={i}
+            className="star"
+            style={{
+              left: `${Math.random() * 100}%`,
+              top: `${Math.random() * 100}%`,
+              animationDelay: `${Math.random() * 5}s`,
+              animationDuration: `${2 + Math.random() * 5}s`,
+            }}
+          />
+        ))}
       </div>
-    </>
-  );
-};
 
-export default Background;
+    </div>
+  );
+}
+
+// export default Background;
--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import "./index.css";
-import "./Components/BodyBackground/Background.css";
+// import "./Components/BodyBackground/Background.css";
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
