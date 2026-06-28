7d5a2caeaf3dc9b683111a48a28996fe1c795c65
nur alam joy <ajnuralam@gmail.com>
2026-06-28 21:20:43 +0600

    write gride layout

---
+++ src/components/BodyBackground/Background.jsx
@@ -0,0 +1,15 @@
+const Background = () => {
+  return (
+    <>
+      {/* Moving Grid */}
+      <div className="grid-bg"></div>
+
+      {/* Glowing Orbs */}
+      <div className="orb orb1"></div>
+      <div className="orb orb2"></div>
+      <div className="orb orb3"></div>
+    </>
+  );
+};
+
+export default Background;
