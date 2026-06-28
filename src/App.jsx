42e5db15bb54468b5d7dca4a8a1bfd6e68aab476
nur alam joy <ajnuralam@gmail.com>
2026-06-28 21:16:12 +0600

    Create Background componenet for body

--- src/App.jsx
+++ src/App.jsx
@@ -6,19 +6,7 @@ const App = () => {
     <>
       <Nav></Nav>
       
-      <div className="pt-20">
-        <section className="h-screen flex items-center justify-center bg-gray-100">
-          <h1 className="text-5xl font-bold">Home Section</h1>
-        </section>
-
-        <section className="h-screen flex items-center justify-center bg-white">
-          <h1 className="text-5xl font-bold">About Section</h1>
-        </section>
-
-        <section className="h-screen flex items-center justify-center bg-gray-100">
-          <h1 className="text-5xl font-bold">Projects Section</h1>
-        </section>
-      </div>
+      
     </>
   );
 }
