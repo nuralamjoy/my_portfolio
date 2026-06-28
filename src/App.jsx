8277b7bbf78e5e88703dc983a0bf9cb5946c0920
nur alam joy <ajnuralam@gmail.com>
2026-06-28 21:39:13 +0600

    import done bg

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import './App.css'
 import Nav from './components/Header/Nav'
+import Background from './components/BodyBackground/Background';
+
 const App = () => {
   return (
     <>
-      <div>
+    
+      <Background></Background>
         <Nav></Nav>
-      </div>
+      
     </>
   );
 }
