d1cb658af50e79c3951bec3bde800114ecb7c129
nur alam joy <ajnuralam@gmail.com>
2026-07-02 21:26:55 +0600

    bg modified

--- src/components/BodyBackground/Background.jsx
+++ src/components/BodyBackground/Background.jsx
@@ -1,24 +1,28 @@
 import "./Background.css";
 
-
-
 export default function Background() {
   return (
     <div className="background">
-
-      <div className="gradient"></div>
+      <div className="aurora"></div>
 
       <div className="grid"></div>
+
+      <div className="noise"></div>
+
+      <div className="vignette"></div>
+
+      <div className="light"></div>
+
+      <div className="particles"></div>
 
       <div className="orb orb1"></div>
       <div className="orb orb2"></div>
       <div className="orb orb3"></div>
       <div className="orb orb4"></div>
-
-      
-
+      <div className="orb orb5"></div>
+      <div className="orb orb6"></div>
+      <div className="orb orb7"></div>
+      <div className="orb orb8"></div>
     </div>
   );
 }
-
-// export default Background;
