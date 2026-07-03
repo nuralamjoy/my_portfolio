9ac2aab8edbe2409cbce0a6d0faa4933be6c8de2
nur alam joy <ajnuralam@gmail.com>
2026-07-03 21:20:37 +0600

    update app,jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import './App.css'
 import Nav from './components/Header/Nav'
 import Background from './components/BodyBackground/Background';
 
+
 const App = () => {
   return (
     <>
     
       <Background></Background>
       <Nav></Nav>
+      
       
     </>
   );
