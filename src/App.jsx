4508239ba016254e738d575346a1dd87692954d1
nur alam joy <ajnuralam@gmail.com>
2026-07-05 15:09:27 +0600

    create from begin

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,11 @@
 import React from 'react'
 import './App.css'
-import Nav from './components/Header/Nav'
-import Background from './components/BodyBackground/Background';
-import Home from './components/Home/Home';
 
 
 const App = () => {
   return (
     <>
-      <Background></Background>
-      <Nav></Nav>
-      <Home></Home>
+      
       
     </>
   );
