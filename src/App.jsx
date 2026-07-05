fb44a224946ebeded6bfd12d81cfb5972a4aeed0
nur alam joy <ajnuralam@gmail.com>
2026-07-05 15:26:57 +0600

    nav change

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,37 @@
-import React from 'react'
-import './App.css'
+import { useEffect, useRef, useState } from "react";
+import React from "react";
+import "./App.css";
 
 import Nav from "./components/Header/Nav.jsx";
+import Background from "./components/BodyBackground/Background";
+
+const SECTION_IDS = [
+  "home",
+  "about",
+  "news",
+  "projects",
+  "skills",
+  "cp",
+  "blogs",
+  "education",
+  "contact",
+];
+
+
 
 const App = () => {
   return (
     <>
-      {/* <Nav
+      <Nav
         scrolled={scrolled}
         activeSection={activeSection}
         dark={dark}
         onToggleTheme={() => setDark((d) => !d)}
-      /> */}
+      />
+
       {/* <Background></Background> */}
-      <Nav></Nav>
-      
     </>
   );
-}
+};
 
-export default App
+export default App;
