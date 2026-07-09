be3d7b0a065cc7eb37e0678b6b1082503d2e77d8
nur alam joy <ajnuralam@gmail.com>
2026-07-09 20:14:52 +0600

    focus card

--- src/components/About/FocusArea.jsx
+++ src/components/About/FocusArea.jsx
@@ -2,8 +2,24 @@
 
 const FocusArea = () => {
   return (
-    <div>FocusArea</div>
-  )
+    <section className="focus-area">
+      <div className="focus-body">
+        <div className="focus-header">
+            title======
+        </div>
+
+        <div className="focus-card">
+          <div>card 1</div>
+          <div>card 1</div>
+          <div>card 1</div>
+          <div>card 1</div>
+          <div>card 1</div>
+          <div>card 1</div>
+        </div>
+        
+      </div>
+    </section>
+  );
 }
 
 export default FocusArea
