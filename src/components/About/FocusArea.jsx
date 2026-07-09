a785e3063f44c93554c339a55cc368dfd15980a4
nur alam joy <ajnuralam@gmail.com>
2026-07-09 20:28:01 +0600

    focus card design

--- src/components/About/FocusArea.jsx
+++ src/components/About/FocusArea.jsx
@@ -2,11 +2,9 @@
 
 const FocusArea = () => {
   return (
-    <section className="focus-area">
+    <section className="focus-area max-w-[1200px] px-[32px] py-[60px] m-auto bg-[#050010] rounded-[20px]">
       <div className="focus-body">
-        <div className="focus-header">
-            title======
-        </div>
+        <div className="focus-header">title======</div>
 
         <div className="focus-card">
           <div>card 1</div>
@@ -16,7 +14,6 @@ const FocusArea = () => {
           <div>card 1</div>
           <div>card 1</div>
         </div>
-        
       </div>
     </section>
   );
