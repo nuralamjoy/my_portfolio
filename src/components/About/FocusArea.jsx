0dac7280b81f52b3aa651ee85f2800a79dd9c1d2
nur alam joy <ajnuralam@gmail.com>
2026-07-10 20:11:07 +0600

    icon design for focus area

--- src/components/About/FocusArea.jsx
+++ src/components/About/FocusArea.jsx
@@ -7,16 +7,34 @@ const FocusArea = () => {
         <div className="focus-header">title======</div>
 
         <div className="focus-card grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 ">
-          <div className="fa-card bg-[var(--card)] rounded-[20px] border border-[var(--border)] px-4 py-6 ">
-            <div className="fa-card-upper">
-              <div className="fa-card-icon flex justify-between">
-                <div className="w-12 h-12 rounded-2xl bg-white/5 border border-white/10 backdrop-blur-md flex items-center justify-center">
-                  <FaBrain className="text-3xl text-violet-400" />
-                </div>
-                <span className=" bg-white/5 border border-white/10 backdrop-blur-md">
-                  Ai/Core
-                </span>
-              </div>
+
+          <div className="fa-card bg-[var(--card)] rounded-[20px] border border-[var(--border)] px-4 py-6 group">
+            <div className="fa-card-upper flex justify-between items-start">
+              
+                  <div className="w-12 h-12 rounded-2xl bg-white/5 border border-white/10 backdrop-blur-md flex items-center justify-center">
+                    <FaBrain className="text-3xl text-violet-400" />
+                  </div>
+
+                  <span
+                    className="
+        text-[11.5px]
+        tracking-[0.02em]
+        text-[var(--muted-dim)]
+        px-[9px]
+        py-[5px]
+        border
+        border-[var(--border)]
+        rounded-md
+        whitespace-nowrap
+        transition-colors
+        duration-300
+        group-hover:text-[var(--accent)]
+        group-hover:border-[var(--accent)]
+      "
+                  >
+                    AI/Core
+                  </span>
+
             </div>
             <h3 className="fa-card-title">Artificial Intelligence</h3>
             <p className="fa-card-desc">
